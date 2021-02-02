925c156257116b71f08cf18d5a507a68403402f5
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 16:35:47 +0000

    Answer correct

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import numpy as np
 
 def convertToBase( N, base ) :
   # Your code goes here
-  
+  number = np.zeros(7)
+  for i in range(7) :
+     ppp = base**(6-i)
+     number[i] = np.floor( N / ppp )
+     N = N - number[i]*ppp
+  return number
 
 # This will print some output that will allow you to test your function
 for b in range(2,10) :
